f0cd1ab02f95dee18281910b695ab5a598ce8895
Alexandre <92374888+AlexandreGirold@users.noreply.github.com>
2023-02-17 15:01:58 +0000

    Evannlebg (#2)
    * Not opti and old solver but file handling, and grid processing in progress... line detection ok, but now I have to filter lines and detect grid.
    
    * removing bad files...
    
    * little update: creating a main file and generalisation of hough_transform
    
    * horizontal filter, vertical filter, line averaging filter
    
    * ugly and not opti, just save not working for the moment
    
    * works but ugly, I can still improve it...
    
    * very ugly, but so powerful... just need img rotation from groleg and it would be perrrrrfect
    
    * Done. Ugly, but done.
    
    * adding img_upgrade to remove more parasites
    
    ---------
    
    Co-authored-by: Evann Marrel <evann.marrel@epita.fr>

---
+++ grid_processing/SDL_utilities.h
@@ -0,0 +1,29 @@
+#ifndef SDL_UTILITIES_H
+#define SDL_UTILITIES_H
+
+// Libraries
+// ========================
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+// ========================
+
+// Files.
+// =========================
+#include "basic_utilities.h"
+#include "doubles_lists.h"
+// =========================
+
+SDL_Surface* load_image(const char path[]);
+SDL_Surface* array_to_surface(int w, int h, int accumulator[]);
+int pixel_to_bit_value(SDL_Surface* surf, Uint32 color);
+void get_array_of_pixels(SDL_Surface* surf, int pixel_bit_array[]);
+void event_loop();
+void show_surface_on_window(SDL_Surface* surf);
+void draw_lines_on_window(struct list* list_rho, struct list* list_theta, SDL_Surface* surf, double surf_diag);
+void draw_points_on_window(struct list* list_x, struct list* list_y, SDL_Surface* surf);
+
+#endif
