637c914f6848d5758810cd5de8e7dee3306ba78c
alexandre.girold <alexandre.girold@epita.fr>
2023-02-17 13:20:17 +0100

    started saves

---
+++ SaveLoadWeights/LoadSave.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define file path for saving the model weights
+const char* file_path = "";
+
+void save_weights(float* weights, int num_weights)
+{
+    
+}
+
