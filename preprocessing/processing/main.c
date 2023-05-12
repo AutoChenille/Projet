652bddb55c135e6e2b54c8e9d936424b566c4dfb
Alexandre <92374888+AlexandreGirold@users.noreply.github.com>
2023-05-12 09:28:48 +0200

    Oleg2 (#27)
    * [SAVE]
    
    * [FIX] fixed corners coordinates detection
    
    * [SAVE] save perspective transform
    
    * [SAVE] save perspective transform 2
    
    * [PT] perspective transform
    
    * [PT] PT finished
    
    * [FIX] fixed découpage
    
    * [SAVE] parameters and filters order
    
    * [OPTI]
    
    * [FIX] Hough Transform
    
    * [NEW] new adaptive thresholding
    
    * [NEW] new adaptive thresholding
    
    * [DELETE] img/
    
    * [DELETE] ag2.png
    
    ---------
    
    Co-authored-by: Oleg KRAJIC <oleg.krajic@epita.fr>

--- preprocessing/processing/main.c
+++ preprocessing/processing/main.c
@@ -4,6 +4,99 @@
 #include "img_upgrade.h"
 #include "../preprocessing/pretreatment.h"
 
+double noise_level(SDL_Surface* surface)
+{
+    int width = surface->w;
+    int height = surface->h;
+
+    double count = 0.0;
+    for (int i = 1; i < width - 1; i++)
+    {
+        for (int j = 1; j < height - 1; j++)
+	{
+            double medium = 0.0;
+
+            // Calculate the average of the neighboring pixels
+            for (int ki = -1; ki <= 1; ki++)
+	    {
+                for (int kj = -1; kj <= 1; kj++)
+		{
+                    Uint32 neighborPixel = ((Uint32*)surface->pixels)[(j + kj) * width + (i + ki)];
+                    Uint8 r, g, b;
+                    SDL_GetRGB(neighborPixel, surface->format, &r, &g, &b);
+                    medium += r;
+                }
+            }
+            medium /= 9;
+
+            // Get the value of the current pixel
+            Uint32 pixel = ((Uint32*)surface->pixels)[j * width + i];
+            Uint8 r, g, b;
+            SDL_GetRGB(pixel, surface->format, &r, &g, &b);
+            double val = 1 - (r / medium);
+            if (val < 0)
+                val *= -1;
+            if (val > 0.5)
+                count++;
+        }
+    }
+
+    return count;
+}
+
+void adaptive_threshold_with_noise(SDL_Surface* surface, const double t)
+{
+    int width = surface->w;
+    int height = surface->h;
+
+    int s2 = fmax(width, height) / 16;
+    unsigned long *thresh = calloc(width * height, sizeof(unsigned long));
+    long sum = 0;
+    unsigned int count = 0;
+    int x1, y1, x2, y2;
+
+    for (int y = 0; y < height; y++) {
+        Uint32 pixel = ((Uint32*)surface->pixels)[y];
+        Uint8 r, g, b;
+        SDL_GetRGB(pixel, surface->format, &r, &g, &b);
+        sum += r;
+        thresh[y] = sum;
+    }
+
+    for (int i = 1; i < width; i++) {
+        sum = 0;
+        for (int j = 0; j < height; j++) {
+            Uint32 pixel = ((Uint32*)surface->pixels)[i * width + j];
+            Uint8 r, g, b;
+            SDL_GetRGB(pixel, surface->format, &r, &g, &b);
+            sum += r;
+            thresh[i * height + j] = thresh[(i - 1) * height + j] + sum;
+        }
+    }
+
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            x1 = fmax(i - s2, 1);
+            x2 = fmin(i + s2, width - 1);
+            y1 = fmax(j - s2, 1);
+            y2 = fmin(j + s2, height - 1);
+            count = (x2 - x1) * (y2 - y1);
+            sum = thresh[x2 * height + y2] - thresh[x2 * height + (y1 - 1)] - thresh[(x1 - 1) * height + y2]
+	      + thresh[(x1 - 1) * height + (y1 - 1)];
+
+            Uint32 pixel = ((Uint32*)surface->pixels)[i * width + j];
+            Uint8 r, g, b;
+            SDL_GetRGB(pixel, surface->format, &r, &g, &b);
+
+            if (r * count < sum * (1.0 - t))
+                ((Uint32*)surface->pixels)[i * width + j] = SDL_MapRGB(surface->format, 0, 0, 0);
+            else
+                ((Uint32*)surface->pixels)[i * width + j] = SDL_MapRGB(surface->format, 255, 255, 255);
+        }
+    }
+
+    free(thresh);
+}
 
 /// @brief Main function to test the detection of sudoku.
 int main(int argc, char** argv)
@@ -57,63 +150,35 @@ int main(int argc, char** argv)
     gaussian_blur(surf_wait, 1, 0.5);
     IMG_SavePNG(surf_wait, "res/gaussian_blur.png");
 
-    //Median Filter
-    //median_filter(surf_wait, 5);
-    //IMG_SavePNG(surf_wait, "res/median.png");
+    //Adaptive Threshold Filter
+    double noise = noise_level(surf_wait);
+    if (noise > 300)
+      noise = 0.5;
+    else
+      noise = 0.15;
+    adaptive_threshold_with_noise(surf_wait, noise);
+    IMG_SavePNG(surf_wait, "res/adaptive_threshold.png");
 
-    //Threshold Filter
-    surf_wait = threshold(surf_wait, 150);
-    IMG_SavePNG(surf_wait, "res/threshold.png");    
-    
+    /*
+    //Threshold
+    threshold(surf_wait, 100);
+    IMG_SavePNG(surf_wait, "res/thresh.png");
+    */
+
+    /*
     //Otsu Adaptative Filter
+    printf("Otsu\n");
     otsu_adaptive_threshold(surf_wait);
     IMG_SavePNG(surf_wait, "res/otsu.png");
+    */
     
     //Canny Edge Detection
     canny(surf_wait);
     IMG_SavePNG(surf_wait, "res/canny.png");
-
-    /*
-    //Invert Filter
-    surf_wait = invert(surf_wait);
-    IMG_SavePNG(surf_wait, "res/invert.png");
-    */
-    
-    /*
-    //Sauvola Filter
-    sauvola(surf_wait, 15, 0.5);
-    IMG_SavePNG(surf_wait, "res/sauvola.png");
-    */
     
     //Dilate Filter
     surf_wait = dilate(surf_wait, 10);
     IMG_SavePNG(surf_wait, "res/dilate.png");
-    
-    /*
-    //Reduce Noise
-    surface = reduce_noise(surface, 3);
-    IMG_SavePNG(surface, "res/reduce_noise.png");
-    
-    //Sauvola Filter
-    sauvola_filter(surface, 15, 0.3);
-    IMG_SavePNG(surface, "sauvola.png");
-    
-    //Median Filter
-    median_filter(surface, 5);
-    IMG_SavePNG(surface, "median.png");
-    
-    //Adaptative Threshold Filter
-    adaptive_threshold(surface, 31, 2);
-    IMG_SavePNG(surface, "res/adaptive_threshold.png");
-
-    //Otsu Adaptative Filter
-    otsu_adaptive_threshold(surface);
-    IMG_SavePNG(surface, "otsu.png");
-    
-    //Sobel Filter
-    sobel_filter(surface);
-    IMG_SavePNG(surface, "sobel.png");
-    */
     // ====================================================
     
 
@@ -133,8 +198,6 @@ int main(int argc, char** argv)
 			    {corners_x[1], corners_y[1]},
 			    {corners_x[2], corners_y[2]},
 			    {corners_x[3], corners_y[3]}};
-    for (int i = 0; i<4; i++)
-      printf("x = %f / y = %f\n", corners[i][0], corners[i][1]);
     // ====================================================
 
     
@@ -156,7 +219,6 @@ int main(int argc, char** argv)
     cut_img = SDL_ConvertSurfaceFormat(cut_img, SDL_PIXELFORMAT_RGBA32, 0);
     // ====================================================
 
-    printf("%s\n", "coucou3");
 
     // HOUGH TRANSFORM - LINES DETECTION
     // ====================================================
@@ -164,14 +226,12 @@ int main(int argc, char** argv)
     struct list* list_theta = list_new();
     // Performs Hough Transform Algorithm.
     hough_transform(surf, &list_theta, &list_rho, 2.1);
-    printf("%s\n", "coucou4");
     // ====================================================
 
 
     // MAXIMUM DETECTION - MATRIX PERSPECTIVE TRANSFORM - CELLS EXTRACTION
     // ====================================================
     grid_detection(list_rho, list_theta, cut_img);
-    printf("%s\n", "coucou5");
     // ====================================================
 
 
