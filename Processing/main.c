f4a576aaa9b67585c2673f4c7dfccb5b074a6fc0
Oleg KRAJIC <oleg.krajic@epita.fr>
2023-05-13 14:18:05 +0200

    [PRETREATMENT] final push

--- Processing/main.c
+++ Processing/main.c
@@ -126,15 +126,15 @@ SDL_Surface** ProcessImage(char *path, int NB_CELLS)
     list_destroy(list_rho);
     list_destroy(list_theta);
 
-    /*
+
     for (int i = 0; i < NB_CELLS * NB_CELLS; i++)
     {
-        // printf("%p\n", ocr_eleven[i]);
-        SDL_FreeSurface(ocr_eleven[i]);
+        IMG_SavePNG(ocr_eleven[i], "res/%i.png", i);
+        //printf("%p\n", ocr_eleven[i]);
+        //SDL_FreeSurface(ocr_eleven[i]);
     }
 
     free(ocr_eleven);
-    */
 
     // End.
     return ocr_eleven;
